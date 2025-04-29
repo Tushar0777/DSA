cb729b9d415b4e9c5259e3cb2b7cef6422556065
Tushar pal <145789688+Tushar0777@users.noreply.github.com>
2025-04-29 18:03:29 +0530

    Update 568-maximum-vacation-days.cpp

--- Graphs/DP on Graphs/time-location-dp/568-maximum-vacation-days.cpp
+++ Graphs/DP on Graphs/time-location-dp/568-maximum-vacation-days.cpp
@@ -35,6 +35,45 @@ public:
 // this is topdown+memo 
 
 
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int n, k;
+    vector<vector<int>> memo;
+
+    int dfs(int city, int week, vector<vector<int>>& flights, vector<vector<int>>& days) {
+        if (week == k) return 0;
+        if (memo[city][week] != -1) return memo[city][week];
+
+        int maxVacation = 0;
+
+        for (int dest = 0; dest < n; ++dest) {
+            // can stay or fly
+            if (city == dest || flights[city][dest] == 1) {
+                int vac = days[dest][week] + dfs(dest, week + 1, flights, days);
+                maxVacation = max(maxVacation, vac);
+            }
+        }
+
+        return memo[city][week] = maxVacation;
+    }
+
+    int maxVacationDays(vector<vector<int>>& flights, vector<vector<int>>& days) {
+        n = flights.size();
+        k = days[0].size();
+        memo = vector<vector<int>>(n, vector<int>(k, -1));
+
+        return dfs(0, 0, flights, days);
+    }
+};
+
+
+
 //now bottom up is below 
 
 
